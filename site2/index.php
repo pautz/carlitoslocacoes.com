39a8a69520e05d75f44ad3a175fb47153d480150
Carlito Pautz <37116894+pautz@users.noreply.github.com>
2025-02-28 15:59:21 -0400

    Create index.php

---
+++ site2/index.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+  <title>Carlito's Locações - Locações e Serviços</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Locação de máquinas e equipamentos para linha de transmissão.">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+  <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+  <style>
+    .carousel-caption {
+      background-color: rgba(0, 0, 0, 0.5); 
+      padding: 10px;
+    }
+  .img-circle-custom {
+    border-radius: 50%;
+    width: 100px; /* Ajuste a largura conforme necessário */
+    height: 100px; /* Ajuste a altura conforme necessário */
+    object-fit: cover;
+  }
+
+  body {
+    font: 22px Montserrat, sans-serif;
+    line-height: 1.8;
+    color: #f5f6f7;
+  }
+
+  p {
+    font-size: 16px;
+  }
+
+  .margin {
+    margin-bottom: 45px;
+  }
+
+  .bg-1, .bg-2, .bg-3, .bg-4, .bg-9 {
+    background-color: #6d0505; /* Cor original */
+    color: #ffffff;
+  }
+
+  .small-font {
+    font: 16px Montserrat, sans-serif;
+  }
+
+  .container-fluid {
+    padding-top: 10px;
+    padding-bottom: 10px;
+  }
+
+  .navbar {
+    padding-top: 3px;
+    padding-bottom: 3px;
+    border: 0;
+    border-radius: 0;
+    margin-bottom: 0;
+    font-size: 11px;
+    letter-spacing: 5px;
+  }
+
+  .navbar-nav li a:hover {
+    color: #1abc9c !important;
+  }
+
+  .carousel-inner > .item > img,
+  .carousel-inner > .item > a > img {
+    display: block;
+    height: 500px;
+    min-width: 100%;
+    width: 100%;
+    max-width: 100%;
+    line-height: 1;
+  }
+
+  input {
+    color: black;
+  }
+
+  @import url("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css");
+  </style>
+</head>
+<body>
+<nav class="navbar navbar-default">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="../">Carlito's Locações</a>
+      <a class="navbar-brand" href="tel:+5555996479747">(55) 9.9647-9747</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="/site2/nossasmaquinas/publicas.php">Nossas Máquinas</a></li>
+        <li><a href="/site/login.php">Entrar</a></li>
+        <li><a href="/contato/">Contato</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="container-fluid bg-1 text-center">
+  <img src="/site2/carlitoschapeu.png" width="300" height="300">
+  <img src="/site2/fontcarlitos.png" width="400" height="400">
+</div>
+
+<div class="container-fluid bg-2 text-center">
+ 
+  
+     <h3>Locação de Equipamentos & Prestação de Serviços</h3><br>
+  
+  <form method="GET" action="../site/msg.php">
+    <label for="id">Enviar por ID:</label>
+    <input type="text" id="id" name="id" required>
+    <button type="submit">Buscar</button><br>
+    </form><br>
+  <a href="/site2/nossasmaquinas/carros.php"><img src="/site2/carrobutton.png" class="img-responsive img-circle margin" style="display:inline" alt="Carros" width="100" height="100"></a>
+  <a href="https://carlitoslocacoes.com/site2/nossasmaquinas/camionetes.php" ><img src="https://carlitoslocacoes.com/site2/camionetebutton.png" class="img-responsive img-circle margin" style="display:inline" alt="facebook" width="100" height="100"></a>
+    <a href="https://carlitoslocacoes.com/site3/cadastro_produto/" ><img src="https://carlitoslocacoes.com/outfit.gif" class="img-responsive img-circle margin" style="display:inline" alt="WhatsApp" width="80" height="100"></a>
+  <a href="https://carlitoslocacoes.com/site2/nossasmaquinas/lanchas.php" ><img src="https://carlitoslocacoes.com/site2/lanchabutton.png" class="img-responsive img-circle margin" style="display:inline" alt="linkedin" width="100" height="100"></a>
+       <a href="/site2/nossasmaquinas/"><img src="https://carlitoslocacoes.com/site2/tratorbutton.png" class="img-responsive img-circle-custom margin" style="display:inline" alt="Cadastre" width="100" height="100"></a><br>
+<form method="GET" action="../site/page.php">
+    <label for="id">Ler por ID:</label>
+    <input type="text" id="id" name="id" required>
+    <button type="submit">Buscar</button>
+    </form><br>
+    <a href="https://carlitoslocacoes.com/site4">Óleo ID Lancha</a><br>
+  <a href="https://github.com/pautz/a_lancha">Óleo ID Lancha - Código</a><br><br>
+  <!-- Continue a adicionar links e imagens como necessário -->
+</div>
+
+<div class="container-fluid bg-3 text-center">
+  <h2 class="margin">Sobre</h2>
+  <p>Carlito's Locações tem seu trabalho voltado para a locação de máquinas e equipamentos para linha de transmissão. Empresa localiza-se no Brasil em Palmeira das Missões, Rio Grande do Sul.</p>
+  <a href="https://api.whatsapp.com/send?phone=5555996479747" class="btn btn-default btn-lg">Fale com Tyu</a><br><br>
+</div>
+
+<div id="myCarousel" class="carousel slide bg-3 text-center">
+  <div class="carousel-inner">
+    <div class="item">
+      <a href="https://api.whatsapp.com/send?phone=5555996479747" class="btn btn-default btn-lg">
+  <img src="https://carlitoslocacoes.com/site2/tratorbutton.png" alt="Fale com Tyu"></a>
+      
+    </div>
+    <div class="item active">
+      <a href="https://github.com/pautz/tr8" class="btn btn-default btn-lg">
+  <img src="allmaq/carlitopautz.png" alt="Projeto TR8 no GitHub">
+  <div class="carousel-caption">
+        <p>Projeto TR8 no GitHub.</p>
+      </div>
+</a>
+    </div>
+    <div class="item">
+        <a href="https://api.whatsapp.com/send?phone=5555996479747" class="btn btn-default btn-lg">
+  <img src="allmaq/t3.png" alt="Retroescavadeira"></a>
+          </div>
+          
+    <div class="item">
+     <a href="https://api.whatsapp.com/send?phone=5555996479747" class="btn btn-default btn-lg">
+  <img src="allmaq/t05.jpg" alt="Trator"></a>
+          </div>
+  </div>    
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+  </a>
+</div>
+<!-- Footer -->
+<footer class="container-fluid bg-4 text-center">
+
+
+
+<p>Desenvolvido por Carlito Veeck Pautz Júnior.</p> 
+<p>Visite o projeto TR8 no GitHub: <a href="https://github.com/pautz/tr8">Projeto TR8 no GitHub<br>
+ <img src="//ipv6.he.net/certification/create_badge.php?pass_name=carlitopautz&amp;badge=1" style="border: 0; width: 128px; height: 128px" alt="Selo de certificação IPv6 para Carlito Pautz">
+<p xmlns:cc="http://creativecommons.org/ns#" xmlns:dct="http://purl.org/dc/terms/"><a property="dct:title" rel="cc:attributionURL" href="http://carlitoslocacoes.com/site2">Carlito's Locações</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName" href="http://carlitoslocacoes.com/pofft">Carlito Veeck Pautz Júnior</a> is licensed under <a href="https://creativecommons.org/licenses/by/4.0/?ref=chooser-v1" target="_blank" rel="license noopener noreferrer" style="display:inline-block;">Creative Commons Attribution 4.0 International<img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1" alt=""><img style="height:22px!important;margin-left:3px;vertical-align:text-bottom;" src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1" alt=""></a></p>
+</footer>
+
+</body>
+</html>
